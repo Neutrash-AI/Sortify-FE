05fa278ebd0d8596ca7c4ec5a93c838256398c92
eben4ya <benaya.imanuela@gmail.com>
2025-03-27 17:29:07 +0700

    feat: Mobile Waste Breakdown Card component

---
+++ src/components/Mobile/MobileWasteBreakdownCard.tsx
@@ -0,0 +1,31 @@
+interface MobileWasteBreakdownCardProps {
+  logo: string;
+  title: string;
+  value: number;
+}
+
+const MobileWasteBreakdownCard: React.FC<MobileWasteBreakdownCardProps> = ({
+  logo,
+  title,
+  value,
+}) => {
+  return (
+    <div className="self-stretch py-3 relative inline-flex justify-center items-center gap-2">
+      <div className="w-8 h-8 relative bg-white dark:bg-[#1f0059] rounded-2xl">
+        <div className="w-8 h-8 left-0 top-0 absolute text-center justify-center text-black text-xl font-normal leading-loose">
+          {logo}
+        </div>
+      </div>
+      <div className="flex-1 inline-flex flex-col justify-start items-start">
+        <div className="self-stretch relative justify-start dark:text-white text-black text-sm font-normal leading-tight">
+          {title}
+        </div>
+      </div>
+      <div className="relative text-right justify-start dark:text-white text-black text-sm font-medium leading-tight">
+        {value}
+      </div>
+    </div>
+  );
+};
+
+export default MobileWasteBreakdownCard;
