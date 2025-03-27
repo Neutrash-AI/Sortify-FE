f7b571021b162abd42f4a482503b04e58ecb29f4
eben4ya <benaya.imanuela@gmail.com>
2025-03-27 17:19:36 +0700

    feat: Mobile Waste List Card component

---
+++ src/components/Mobile/MobileWasteListCard.tsx
@@ -0,0 +1,24 @@
+interface MobileWasteListCardProps {
+  desc1: string;
+  value1: number;
+  desc2: string;
+  value2: number;
+}
+
+import MobileWasteCard from "./MobileWasteCard";
+
+const MobileWasteListCard: React.FC<MobileWasteListCardProps> = ({
+  desc1,
+  value1,
+  desc2,
+  value2,
+}) => {
+  return (
+    <div className="self-stretch inline-flex justify-start items-start gap-2">
+      <MobileWasteCard desc={desc1} value={value1} />
+      <MobileWasteCard desc={desc2} value={value2} />
+    </div>
+  );
+};
+
+export default MobileWasteListCard;
