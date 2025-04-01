59462886a3afd84cdfd4ad3b8f0f710ebc987766
eben4ya <benaya.imanuela@gmail.com>
2025-04-01 10:14:17 +0700

    fix: Desktop Layout component size
    change px size into vw

--- src/components/Desktop/DesktopLayout.tsx
+++ src/components/Desktop/DesktopLayout.tsx
@@ -26,17 +26,17 @@ const DesktopLayout: React.FC<DesktopLayoutProps> = ({
   return (
     <main
       className="
-        hidden md:block
-        w-screen md:w-[820px] lg:w-screen lg:min-h-screen
-        min-h-[100vmax] relative lg:flex lg:flex-col lg:items-center lg:justify-center
-        bg-[#f7f6f9] dark:bg-[#15003f]
-        lg:overflow-hidden md:overflow-y-scroll
-      "
+      hidden md:block
+      w-screen md:w-[43.1vw] lg:w-screen lg:min-h-screen
+      min-h-[100vmax] relative lg:flex lg:flex-col lg:items-center lg:justify-center
+      bg-[#f7f6f9] dark:bg-[#15003f]
+      lg:overflow-hidden md:overflow-y-scroll
+    "
     >
       {/* Navbar */}
       <DesktopNavbar />
 
-      {/* Toggle Device Connection*/}
+      {/* Toggle Device Connection */}
       <DesktopToggle
         type="DeviceConnection"
         mode={isConnected}
@@ -56,11 +56,11 @@ const DesktopLayout: React.FC<DesktopLayoutProps> = ({
       {/* Live Camera */}
       <div
         className="
-          rounded-md border dark:border-transparent border-[#e6e6e6]
-          inline-flex justify-center items-center overflow-hidden
-          lg:w-[520px] lg:h-[596px] lg:absolute lg:left-[199px] lg:top-[232px]
-          md:w-[520px] md:h-[458px] md:absolute md:left-[219px] md:top-[149px]
-        "
+        rounded-md border dark:border-transparent border-[#e6e6e6]
+        inline-flex justify-center items-center overflow-hidden
+        lg:w-[27.3vw] lg:h-[31.3vw] lg:absolute lg:left-[10.5vw] lg:top-[12.2vw]
+        md:w-[27.3vw] md:h-[24.1vw] md:absolute md:left-[11.5vw] md:top-[7.8vw]
+      "
       >
         <LiveCamera socketUrl={socketUrl} width={520} height={400} />
       </div>
