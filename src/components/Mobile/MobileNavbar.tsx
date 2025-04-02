b141578b49ea3c8d392b776d5ea70b78888bec21
eben4ya <benaya.imanuela@gmail.com>
2025-04-02 12:49:20 +0700

    fix: Mobile Navbar component size
    change px size into vw

--- src/components/Mobile/MobileNavbar.tsx
+++ src/components/Mobile/MobileNavbar.tsx
@@ -2,14 +2,14 @@
 
 const MobileNavbar: React.FC = () => {
   return (
-    <div className="self-stretch dark:bg-[#0a0024] bg-[#4caf50] shadow-[0px_0px_6px_0px_rgba(0,0,0,0.12)] flex flex-col justify-start items-start">
-      <div className="self-stretch pl-4 pr-2 py-3 inline-flex justify-start items-center gap-2">
-        <div className="w-[22px] h-6 relative overflow-hidden">
-          <div className="w-[15px] h-0 left-[3px] top-[6px] absolute ring-2 ring-white" />
-          <div className="w-[15px] h-0 left-[3px] top-[12px] absolute ring-2 ring-white" />
-          <div className="w-[15px] h-0 left-[3px] top-[18px] absolute ring-2 ring-white" />
+    <div className="self-stretch dark:bg-[#0a0024] bg-[#4caf50] shadow-[0vw_0vw_1.67vw_0vw_rgba(0,0,0,0.12)] flex flex-col justify-start items-start">
+      <div className="self-stretch pl-[4.44vw] pr-[2.22vw] py-[3.33vw] inline-flex justify-start items-center gap-[2.22vw]">
+        <div className="w-[6.11vw] h-[6.67vw] relative overflow-hidden">
+          <div className="w-[4.17vw] h-0 left-[0.83vw] top-[1.67vw] absolute ring-[0.35vw] ring-white" />
+          <div className="w-[4.17vw] h-0 left-[0.83vw] top-[3.26vw] absolute ring-[0.35vw] ring-white" />
+          <div className="w-[4.17vw] h-0 left-[0.83vw] top-[5.00vw] absolute ring-[0.35vw] ring-white" />
         </div>
-        <div className="flex-1 relative justify-start text-white text-xl font-bold font-['Roboto'] leading-normal">
+        <div className="flex-1 relative justify-start text-white text-[5.4vw] font-bold font-['Roboto'] leading-normal">
           Sortify - Smart Bin
         </div>
       </div>
