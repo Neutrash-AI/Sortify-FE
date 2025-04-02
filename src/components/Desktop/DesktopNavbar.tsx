ddf1bd7e3f48c9a53ead4598e991c8452e55de9e
eben4ya <benaya.imanuela@gmail.com>
2025-04-02 11:27:01 +0700

    fix: Tablet Navbar component size
    change px size into vw

--- src/components/Desktop/DesktopNavbar.tsx
+++ src/components/Desktop/DesktopNavbar.tsx
@@ -6,43 +6,43 @@ const DesktopNavbar: React.FC = () => {
       className="
       p-[1.05vw] absolute left-0 top-0 shadow-[0vw_0vw_0.3vw_0vw_rgba(0,0,0,0.12)]
       inline-flex justify-center items-center gap-[1.05vw] overflow-hidden
-      lg:w-[100vw] lg:h-[3.0vw] lg:bg-[#4caf50] dark:bg-[#0a0024]
-      md:w-[43.1vw] md:h-[3.0vw] md:bg-[#4caf50]
+      w-[100vw] lg:h-[3.0vw] lg:bg-[#4caf50] dark:bg-[#0a0024]
+       md:h-[7.07vw] md:bg-[#4caf50]
     "
     >
       <div
         className="
             relative overflow-hidden
             lg:w-[1.2vw] lg:h-[1.26vw]
-            md:w-[1.2vw] md:h-[1.26vw]
+            md:w-[2.68vw] md:h-[2.92vw]
           "
       >
         <div
           className="
               absolute left-[0.2vw]
-              lg:top-[0.3vw] md:top-[0.3vw]
+              lg:top-[0.3vw] md:top-[1.463vw]
               lg:w-[0.8vw] lg:h-0 lg:ring-[0.1vw] lg:ring-white
-              md:w-[0.8vw] md:h-0 md:ring-[0.1vw] md:ring-white
+              md:w-[1.83vw] md:h-0 md:ring-[0.9vw] md:ring-white
             "
         />
         <div
           className="
-              absolute left-[0.2vw]
-              lg:top-[0.6vw] md:top-[0.6vw]
-              lg:w-[0.8vw] lg:h-0 lg:ring-[0.1vw] lg:ring-white
-              md:w-[0.8vw] md:h-0 md:ring-[0.1vw] md:ring-white
-            "
+         absolute left-[0.2vw]
+         lg:top-[0.3vw] md:top-[1.463vw]
+         lg:w-[0.8vw] lg:h-0 lg:ring-[0.1vw] lg:ring-white
+         md:w-[1.83vw] md:h-0 md:ring-[0.9vw] md:ring-white
+       "
         />
         <div
           className="
-              absolute left-[0.2vw]
-              lg:top-[0.9vw] md:top-[0.9vw]
-              lg:w-[0.8vw] lg:h-0 lg:ring-[0.1vw] lg:ring-white
-              md:w-[0.8vw] md:h-0 md:ring-[0.1vw] md:ring-white
-            "
+          absolute left-[0.2vw]
+          lg:top-[0.3vw] md:top-[1.463vw]
+          lg:w-[0.8vw] lg:h-0 lg:ring-[0.1vw] lg:ring-white
+          md:w-[1.83vw] md:h-0 md:ring-[0.9vw] md:ring-white
+        "
         />
       </div>
-      <div className="flex-1 relative text-white text-[1.5vw] font-medium font-['Roboto'] leading-[1.89vw]">
+      <div className="flex-1 relative text-white md:text-[3.4vw] lg:text-[1.5vw] font-medium font-['Roboto'] md:leading-[4.39vw] lg:leading-[1.89vw]">
         Sortify - Smart Bin
       </div>
     </div>
