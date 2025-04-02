a89e0f13a3cbf33a5a2a5abe52c35dae7c22990f
eben4ya <benaya.imanuela@gmail.com>
2025-04-02 19:51:26 +0700

    feat: circle chart

--- src/components/Desktop/DesktopWasteCompotition.tsx
+++ src/components/Desktop/DesktopWasteCompotition.tsx
@@ -1,11 +1,30 @@
-const DesktopWasteCompotition = () => {
+import {
+  PieChart,
+  Pie,
+  Cell,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const data = [
+  { name: "Plastic", value: 20 },
+  { name: "Paper", value: 20 },
+  { name: "Metal", value: 20 },
+  { name: "Glass", value: 30 },
+  { name: "Other", value: 10 },
+];
+
+const COLORS = ["#4caf50", "#2196f3", "#ff9800", "#9c27b0", "#f44336"];
+
+const DesktopWasteComposition = () => {
   return (
     <div
       className="
-      hidden md:hidden lg:inline-flex
-      lg:w-[25.9vw] lg:h-[23.1vw] lg:absolute lg:left-[41.1vw] lg:top-[20.4vw]
-      flex-col justify-start items-start gap-[0.21vw]
-    "
+        hidden md:hidden lg:inline-flex
+        lg:w-[25.9vw] lg:h-[23.1vw] lg:absolute lg:left-[41.1vw] lg:top-[20.4vw]
+        flex-col justify-start items-start gap-[0.21vw]
+      "
     >
       <div className="w-full flex-1 p-[1.05vw] bg-white dark:bg-transparent rounded-md border border-black dark:border-[#d9d9d9] flex flex-col justify-start items-start gap-[0.63vw]">
         <div className="w-full text-black dark:text-white text-[1.2vw] font-medium font-['Roboto'] leading-[1.47vw]">
@@ -14,10 +33,30 @@ const DesktopWasteCompotition = () => {
         <div className="w-full text-black dark:text-[#e0e0e1] text-[0.85vw] font-normal font-['Roboto'] leading-normal">
           Percentage
         </div>
-        <div className="w-full flex-1 relative">
-          <div className="w-[14.7vw] h-[14.7vw] absolute left-[4.57vw] top-[0.21vw] bg-black dark:bg-[#d9d9d9] rounded-full" />
-          <div className="w-[14.7vw] h-[14.7vw] absolute left-[4.57vw] top-[0.21vw] bg-black dark:bg-white rounded-full" />
-          <div className="w-[14.7vw] h-[14.7vw] absolute left-[4.57vw] top-[0.21vw] bg-black/50 rounded-full" />
+        <div className="w-full h-[14.7vw] flex items-center justify-center">
+          <ResponsiveContainer width="100%" height="100%">
+            <PieChart>
+              <Pie
+                data={data}
+                cx="50%"
+                cy="50%"
+                innerRadius="40%"
+                outerRadius="70%"
+                fill="#8884d8"
+                paddingAngle={3}
+                dataKey="value"
+              >
+                {data.map((_, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index % COLORS.length]}
+                  />
+                ))}
+              </Pie>
+              <Tooltip />
+              <Legend />
+            </PieChart>
+          </ResponsiveContainer>
         </div>
         <div className="w-full text-right text-black dark:text-[#e0e0e1] text-[0.85vw] font-normal font-['Roboto'] leading-normal">
           Types of Waste
@@ -27,4 +66,4 @@ const DesktopWasteCompotition = () => {
   );
 };
 
-export default DesktopWasteCompotition;
+export default DesktopWasteComposition;
