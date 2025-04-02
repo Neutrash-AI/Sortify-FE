88890cfea04cb1039b5c13a53658f4f9a672937c
eben4ya <benaya.imanuela@gmail.com>
2025-04-02 10:22:57 +0700

    fix: Desktop Sidebar component size
    change px size into vw

--- src/components/Desktop/DesktopSidebar.tsx
+++ src/components/Desktop/DesktopSidebar.tsx
@@ -4,29 +4,29 @@ const DesktopSidebar: React.FC = () => {
   return (
     <div
       className="
-        py-3 absolute left-0 top-[58px] inline-flex flex-col justify-start items-center overflow-hidden
-        lg:w-[159px] lg:h-[1022px] lg:bg-white dark:bg-[#1f0059]
-        md:w-[159px] md:h-[1122px] md:bg-white
-      "
+    py-[0.63vw] absolute left-0 top-[3.04vw] inline-flex flex-col justify-start items-center overflow-hidden
+    lg:w-[8.34vw] lg:h-[53.66vw] lg:bg-white dark:bg-[#1f0059]
+    md:w-[8.34vw] md:h-[58.90vw] md:bg-white
+  "
     >
-      <div className="w-full px-5 py-4 inline-flex justify-center items-center gap-3">
+      <div className="w-full px-[1.05vw] py-[0.84vw] inline-flex justify-center items-center gap-[0.63vw]">
         <div
           className="
-            relative rounded-xl
-            lg:w-5 lg:h-5 lg:bg-black/5 lg:dark:bg-[#0a0024]/50
-            md:w-5 md:h-5 md:bg-black/5
-          "
+        relative rounded-xl
+        lg:w-[1.05vw] lg:h-[1.05vw] 
+        md:w-[1.05vw] md:h-[1.05vw] 
+      "
         >
-          <div className="absolute left-[-2px] top-[-2px] w-6 h-6 flex items-center justify-center text-center text-black dark:text-white text-[15px] font-bold font-['Roboto']">
+          <div className="absolute left-[-0.11vw] top-[-0.11vw] w-[1.26vw] h-[1.26vw] flex items-center justify-center text-center text-black dark:text-white text-[0.79vw] font-bold font-['Roboto']">
             📷
           </div>
         </div>
         <div
           className="
-            flex-1 text-base font-medium font-['Roboto'] leading-tight
-            lg:text-[#212121] dark:lg:text-[#e0e0e0]
-            md:text-[#212121]
-          "
+        flex-1 text-base font-medium font-['Roboto'] leading-tight
+        lg:text-[#212121] dark:lg:text-[#e0e0e0]
+        md:text-[#212121]
+      "
         >
           Camera 1
         </div>
